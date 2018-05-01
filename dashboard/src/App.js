f76e3c1615337a0b8891e32cd90ceee7cc1af744
Filipe <filipegroh@gmail.com>
2018-04-30 21:22:30 -0300

    mockup 2

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -10,10 +10,6 @@ const SectionMain = SectionSegmented.extend`flex: 0 0 auto; background-color:#32
 const SectionSec = SectionSegmented.extend`flex: 0 0 auto; padding: 0 10px; background-color:#4fe0e3 !important;`;
 const SectionTerc = SectionSegmented.extend`color:grey; padding: 0 0 0 10px;`;
 
-const SpanBox = styled.div`padding:5px;`;
-const TopSpan = styled.p`margin: 0 0 10px; font-weight:bold;`;
-const BottomSpan = styled.p`margin: 0px;`;
-
 const ProfilePic = styled.i`font-size:64px; background: whitesmoke;`;
 const Icon = styled.i`margin:10px;`;
 const IconSmall = styled.i`margin:10px; font-size:75%;`;
@@ -22,17 +18,21 @@ const ContentBox = styled.div`padding: ${1 / 12 * 100 / 2}vw ${1 / 12 * 100 / 2}
 const CardPermanent = styled.div`min-height: ${3 / 12 * 100 / 2}vw; margin-bottom:${1 + 1 / 12 * 100 / 2}vw; padding:10px;`;
 const Card = CardPermanent.extend`min-height: ${3 / 12 * 100 / 2}vw; margin-bottom:${1 + 1 / 12 * 100 / 2}vw;`;
 
-const Row = styled.div`min-height: ${3 / 12 * 100 / 2/3}vw;`;
-const FlexRow = Row.extend`display: flex; justify-content: space-between;`;
-const FlexSectionLeft = styled.section`justify-content: flex-start; order: -1;`
-const FlexSectionRight = styled.section`justify-content: flex-end; order: 1;`
+const Row = styled.div`min-height: ${3 / 12 * 100 / 2 / 3}vw;`;
+const FlexRow = Row.extend`display: flex; justify-content: space-between; flex-wrap: wrap;`;
+const FlexSectionLeft = styled.section`justify-content: flex-start; order: -1;`;
+const FlexSectionRight = styled.section`justify-content: flex-end; order: 1;`;
+const FlexSection = FlexSectionLeft;
 
+const SpanBox = styled.div`padding:5px;`;
+const P = styled.p`margin: 0 0 5px; `
+const T1 = styled.span`font-size:125%; font-weight:bold; color:#959da5;`;
+const T2 = styled.span`font-weight:bold; color:#959da5;`;
+const T3 = styled.span`font-size:75%; color:#959da5;`;
 
-
-const T1 = styled.span`font-size:125%; font-weight:bold;`
-const T2= styled.span`font-weight:bold;`
-const T3 = styled.span`font-size:75%;`
-
+const NoShow360 = styled.span`@media (max-width: 360px) { display:none }; display:inherit;`;
+const NoShow480 = styled.span`@media (max-width: 480px) { display:none }; display:inherit;`;
+const NoShow600 = styled.span`@media (max-width: 600px) { display:none }; display:inherit;`;
 
 class App extends React.Component {
 
@@ -47,19 +47,25 @@ class App extends React.Component {
             <SectionSec className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start mdc-theme--secondary-bg">
               <span>DashBoard</span>
             </SectionSec>
+
             <SectionTerc className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start mdc-theme--background">
-              <Icon className="material-icons">notifications</Icon>
-              <Icon className="material-icons">chat_bubble_outline</Icon>
-              <Icon className="material-icons">settings</Icon>
-              <Icon className="material-icons">search</Icon>
+              <NoShow360>
+                <Icon className="material-icons">notifications</Icon>
+                <Icon className="material-icons">chat_bubble_outline</Icon>
+                <Icon className="material-icons">settings</Icon>
+                <Icon className="material-icons">search</Icon>
+              </NoShow360>
             </SectionTerc>
-            <SectionTerc className="mdc-top-app-bar__section mdc-top-app-bar__section--align-end mdc-theme--background">
-              <SpanBox>
-                <TopSpan>Usuário</TopSpan>
-                <BottomSpan>Gerente</BottomSpan>
-              </SpanBox>
-              <ProfilePic className="material-icons">face</ProfilePic>
+            <SectionTerc className="mdc-top-app-bar__section mdc-top-app-bar__section--align-end mdc-theme--background" style={{ flex: '0 0 auto' }}>
+              <NoShow480>
+                <SpanBox>
+                  <P>Usuário</P>
+                  <P>Gerente</P>
+                </SpanBox>
+                <ProfilePic className="material-icons">face</ProfilePic>
+              </NoShow480>
             </SectionTerc>
+
           </div>
         </header>
 
@@ -98,43 +104,39 @@ class App extends React.Component {
           <CardPermanent className="mdc-card">
             <FlexRow>
               <FlexSectionLeft><T1>Ganhos Estimados</T1> </FlexSectionLeft>
-              <FlexSectionRight><T2>Código do Cliente:</T2><T3>XJGHK-12J1J.SS562S.45-A</T3></FlexSectionRight>            
+              <FlexSectionRight><NoShow600><T2>Código do Cliente:</T2><T3>XJGHK-12J1J.SS562S.45-A</T3></NoShow600></FlexSectionRight>
             </FlexRow>
             <FlexRow>
-              <FlexSectionLeft><SpanBox><TopSpan>Text</TopSpan><BottomSpan>Txet</BottomSpan></SpanBox></FlexSectionLeft>
-              <FlexSectionLeft><SpanBox><TopSpan>Text</TopSpan><BottomSpan>Txet</BottomSpan></SpanBox></FlexSectionLeft>
-              <FlexSectionLeft><SpanBox><TopSpan>Text</TopSpan><BottomSpan>Txet</BottomSpan></SpanBox></FlexSectionLeft>
-              <FlexSectionLeft><SpanBox><TopSpan>Text</TopSpan><BottomSpan>Txet</BottomSpan></SpanBox></FlexSectionLeft>
-              <FlexSectionLeft><SpanBox><TopSpan>Text</TopSpan><BottomSpan>Txet</BottomSpan></SpanBox></FlexSectionLeft>
+              <FlexSection><SpanBox>
+                <P>Total do dia:</P>
+                <P>R$2.504,12</P>
+              </SpanBox></FlexSection>
+              <FlexSection><SpanBox>
+                <P><NoShow360>Para o </NoShow360>dia atual:</P>
+                <P>R$52,12</P>
+              </SpanBox></FlexSection>
+              <FlexSection><SpanBox>
+                <P><NoShow360>Para o </NoShow360>dia anterior:</P>
+                <P>R$89,10</P>
+              </SpanBox></FlexSection>
+              <FlexSection><SpanBox>
+                <P><NoShow360>Para o </NoShow360>mês atual:</P>
+                <P>R$875,25</P>
+              </SpanBox></FlexSection>
+              <FlexSection><SpanBox>
+                <P><NoShow360>Para o </NoShow360>mês passado:</P>
+                <P>R$1.750,85</P>
+              </SpanBox></FlexSection>
             </FlexRow>
-            <Row><hr/></Row>
+            <Row><hr /></Row>
             <FlexRow>
               <FlexSectionLeft>DropDown Here!</FlexSectionLeft>
-              <FlexSectionRight>BUTTON</FlexSectionRight>            
+              <FlexSectionRight>BUTTON</FlexSectionRight>
             </FlexRow>
           </CardPermanent>
-          
+
           <Card className="mdc-card">
-            <Row>1</Row>
-            <Row>2</Row>
-            <Row>3</Row>
-            <Row>4</Row>
-            <Row>3</Row>
-          </Card>
-          <Card className="mdc-card">
-            <Row>1</Row>
-            <Row>2</Row>
-            <Row>3</Row>
-          </Card>
-          <Card className="mdc-card">
-            <Row>1</Row>
-            <Row>2</Row>
-            <Row>3</Row>
-          </Card>
-          <Card className="mdc-card">
-            <Row>1</Row>
-            <Row>2</Row>
-            <Row>3</Row>
+
           </Card>
         </ContentBox>
       </div>
